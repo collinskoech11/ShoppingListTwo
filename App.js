75ec9b7aff224105ea6be661e8e0496de6d598ee
Collins Koech <collinskoechck34@gmail.com>
2020-10-09 13:20:22 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'App.css' ;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Start App</Text>
       <StatusBar style="auto" />
+      <div className="nav">
+        Hello React Native
+      </div>
     </View>
   );
 }
@@ -14,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
